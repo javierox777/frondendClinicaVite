581736964feb5a447a16b2cc0a8f4e26eadd8003
Andres Miranda <andres.miranda.dev@gmail.com>
2024-05-07 14:37:11 +0900

    add card to budgets visualizer

--- src/pages/budgets/BudgetVisualizer.tsx
+++ src/pages/budgets/BudgetVisualizer.tsx
@@ -1,4 +1,10 @@
-import { Container, Grid, TablePagination, Typography } from '@mui/material';
+import {
+  Card,
+  Container,
+  Grid,
+  TablePagination,
+  Typography,
+} from '@mui/material';
 import React, { useState } from 'react';
 import { useThemeContext } from '../../componemts/themeContext';
 import { Budget } from '../../interfaces/Budget';
@@ -47,68 +53,80 @@ const BudgetVisualizer = ({ budgets }: Props) => {
     setPage(0);
   };
 
+  if (budgets.length === 0) {
+    return (
+      <Container>
+        <Typography>No hay presupuestos que mostrar</Typography>
+      </Container>
+    );
+  }
+
   return (
     <Container>
-      <Grid container>
-        <Grid
-          item
-          xs={12}
-          sx={{
-            backgroundColor:
-              mode === 'light'
-                ? colors.lightModeTableHead
-                : colors.darkModeTableHead,
-          }}
-        >
-          <Grid container direction="row" justifyContent="space-between">
-            {headings.map((h: any) => {
+      <Card elevation={3} sx={{ padding: 3 }}>
+        <Grid container>
+          <Grid
+            item
+            xs={12}
+            sx={{
+              backgroundColor:
+                mode === 'light'
+                  ? colors.lightModeTableHead
+                  : colors.darkModeTableHead,
+            }}
+          >
+            <Grid container direction="row" justifyContent="space-between">
+              {headings.map((h: any) => {
+                return (
+                  <Grid item key={h.id} xs={3}>
+                    <Typography sx={{ fontWeight: 'bold' }}>
+                      {h.label}
+                    </Typography>
+                  </Grid>
+                );
+              })}
+            </Grid>
+          </Grid>
+          {budgets
+            ?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            .map((b: Budget) => {
               return (
-                <Grid item key={h.id} xs={3}>
-                  <Typography sx={{ fontWeight: 'bold' }}>{h.label}</Typography>
+                <Grid
+                  container
+                  key={b.id}
+                  justifyContent="space-between"
+                  sx={{ paddingBlock: 3 }}
+                  className="hover:scale-[1.01] transition-all duration-500 cursor-pointer"
+                >
+                  <Grid item xs={3}>
+                    <Typography>
+                      {b.persona.nombre1} {b.persona.apellPat}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <Typography>{b.persona.rut}</Typography>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <Typography>
+                      {new Date(b.fechaRegistro).toLocaleDateString()}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <Typography>{b.presupuestoTipo.nombre}</Typography>
+                  </Grid>
                 </Grid>
               );
             })}
-          </Grid>
+          <TablePagination
+            page={page}
+            onPageChange={handleChangePage}
+            count={budgets?.length}
+            component="div"
+            rowsPerPage={rowsPerPage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
         </Grid>
-        {budgets
-          ?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-          .map((b: Budget) => {
-            return (
-              <Grid
-                container
-                key={b.id}
-                justifyContent="space-between"
-                sx={{ paddingBlock: 3 }}
-                className="hover:scale-[1.01] transition-all duration-500 cursor-pointer"
-              >
-                <Grid item xs={3}>
-                  <Typography>
-                    {b.persona.nombre1} {b.persona.apellPat}
-                  </Typography>
-                </Grid>
-                <Grid item xs={3}>
-                  <Typography>{b.persona.rut}</Typography>
-                </Grid>
-                <Grid item xs={3}>
-                  <Typography>
-                    {new Date(b.fechaRegistro).toLocaleDateString()}
-                  </Typography>
-                </Grid>
-                <Grid item xs={3}>
-                  <Typography>{b.presupuestoTipo.nombre}</Typography>
-                </Grid>
-              </Grid>
-            );
-          })}
-        <TablePagination
-          page={page}
-          onPageChange={handleChangePage}
-          count={budgets?.length}
-          component="div"
-          rowsPerPage={rowsPerPage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
-      </Grid>
+      </Card>
     </Container>
   );
 };
