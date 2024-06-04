54ecdc74de48b8a226668817fbf78d569fb25cda
Andres Miranda <andres.miranda.dev@gmail.com>
2024-06-04 16:18:32 +0900

    delete unused appointment form

--- src/pages/appointments/AppointmentsPage.tsx
+++ src/pages/appointments/AppointmentsPage.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import AppointmentsCalendar from './AppointmentsCalendar';
 import { Grid } from '@mui/material';
-import AppointmentForm from './AppointmentForm';
 
 const AppointmentsPage = () => {
   return (
     <Grid container spacing={3}>
-      {/* <Grid item xs={12} sm={12} md={12} lg={2} xl={2}>
-        <AppointmentForm />
-      </Grid> */}
       <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
         <AppointmentsCalendar />
       </Grid>
