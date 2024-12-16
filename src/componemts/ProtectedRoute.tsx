d1faeeac0d159ebf5bf6c5a92168d55c3dae92f8
Andres Miranda <andres.miranda.dev@gmail.com>
2024-12-16 21:11:28 +0900

    arreglar loading screen

--- src/componemts/ProtectedRoute.tsx
+++ src/componemts/ProtectedRoute.tsx
@@ -1,6 +1,8 @@
 import { Navigate } from 'react-router-dom';
 import { useUser } from '../auth/userContext'; // Ajusta según tu contexto de usuario
 import React from 'react';
+import logo from '../../public/logo.png';
+import { Grid, LinearProgress, Typography } from '@mui/material';
 
 interface ProtectedRouteProps {
   children: React.ReactNode;
@@ -11,7 +13,34 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
 
   if (loading) {
     // Mostrar un indicador de carga mientras se verifica la autenticación
-    return <div>Loading...</div>;
+    return (
+      <Grid container justifyContent="center" alignItems="center">
+        <Grid item xs={12}>
+          <div className="flex  justify-center">
+            <img src={logo} alt="logo" height={300} width={400} />
+          </div>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography
+            style={{ textAlign: 'center', color: '#53B0F3', fontSize: 25 }}
+          >
+            Clínica Dental
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography
+            style={{ textAlign: 'center', color: '#e895da', fontSize: 25 }}
+          >
+            Amania
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <div className="mx-32">
+            <LinearProgress />
+          </div>
+        </Grid>
+      </Grid>
+    );
   }
 
   const isAuthenticated = user !== null;
